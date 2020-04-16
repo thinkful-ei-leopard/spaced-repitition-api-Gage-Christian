35881fe1722b7690e8ac8003bd6dba08733587e6
gage117 <57643501+gage117@users.noreply.github.com>
2020-04-16 13:47:54 -0500

    Adjusted files for deployment

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
+  "ssl": !!process.env.SSL,
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.MIGRATION_DB_NAME,
